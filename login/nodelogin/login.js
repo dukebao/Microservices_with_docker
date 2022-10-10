2c1b62f5f21575eaff8b7002e1e4d51dcf2e7769
dukebao <78891975+dukebao@users.noreply.github.com>
2022-10-10 11:39:41 -0700

    initial commit , db volume not created !

---
+++ login/nodelogin/login.js
@@ -0,0 +1,59 @@
+const mysql = require('mysql');
+const express = require('express');
+const session = require('express-session');
+const path = require('path');
+
+function return_True() {
+    return true;
+}
+
+
+const app = express();
+app.use(session({
+    secret: 'secret',
+    resave: true,
+    saveUninitialized: true
+}));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'static')));
+
+// http://localhost:3000/
+app.get('/', function (request, response) {
+    // Render login template
+    response.sendFile(path.join(__dirname + '/login.html'));
+});
+
+// http://localhost:3000/auth
+app.post('/auth', function (request, response) {
+    // Capture the input fields
+    let username = request.body.username;
+    let password = request.body.password;
+    // Ensure the input fields exists and are not empty
+    if (username == password) {
+        // for now, just always redirect to /home
+        request.session.loggedin = true;
+        request.session.username = username;
+        // Redirect to home page
+        response.redirect('http://localhost:6868/input?username=' + username);
+    } else {
+        // response.send('Login Failed !!!!');
+        // response.end();
+        response.redirect('/');
+    };
+});
+
+// http://localhost:3000/home
+app.get('/home', function (request, response) {
+    // If the user is loggedin
+    if (request.session.loggedin) {
+        // Output username
+        response.send('Welcome back, ' + request.session.username + '!');
+    } else {
+        // Not logged in
+        response.send('Please login to view this page!');
+    }
+    response.end();
+});
+
+app.listen(3000);
